5e0de2bc7e68aebb72bc47af5b561879930232a4
jelliyjane <rean5123@naver.com>
2019-03-04 19:20:50 +0900

    please name error

--- secondproject/wsgi.py
+++ secondproject/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "locallibrary.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secondproject.settings")
 
 application = get_wsgi_application()
 
